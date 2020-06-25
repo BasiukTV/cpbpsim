7a926b7790a79cedb6c770a2a18972d2676252ef
Taras Basiuk <BasiukTV@gmail.com>
2020-06-24 22:52:42 -0500

    Implementing the to-file persistance of the probabilistic data migration policy.

--- cpbpsim/data_migration_policy/data_migration_policy.py
+++ cpbpsim/data_migration_policy/data_migration_policy.py
@@ -35,49 +35,55 @@ class ProbabilityBasedDataMigrationPolicy(AbstractDataMigrationPolicy):
         and a uniform distribution of a random variable
     """
 
-    def __init__(self, config_params, init_from_file=None):
-
+    def __init__(self, config_params=None, init_from_file=None):
         if init_from_file:
-            raise NotImplementedError
+            # If the file is given, populate the tiers, tier_index, data_admission_matrix and data_eviction_matrix from the file.
+            import json
+            with open(init_from_file) as f:
+                persisted_state = json.load(f)
+                self.tiers = persisted_state['tiers']
+                self.tier_index = persisted_state['tier_index']
+                self.data_admission_matrix = persisted_state['data_admission_matrix']
+                self.data_eviction_matrix = persisted_state['data_eviction_matrix']
+        else:
+            # Unpacking expected config_params
+            assert "tiers" in config_params, "config_params is expected to contain 'tiers'"
+            tiers = config_params["tiers"]
+            assert "data_admission_matrix" in config_params, "config_params is expected to contain 'data_admission_matrix'"
+            data_admission_matrix = config_params["data_admission_matrix"]
+            assert "data_eviction_matrix" in config_params, "config_params is expected to contain 'data_eviction_matrix'"
+            data_eviction_matrix = config_params["data_eviction_matrix"]
 
-        # Unpacking expected config_params
-        assert "tiers" in config_params, "config_params is expected to contain 'tiers'"
-        tiers = config_params["tiers"]
-        assert "data_admission_matrix" in config_params, "config_params is expected to contain 'data_admission_matrix'"
-        data_admission_matrix = config_params["data_admission_matrix"]
-        assert "data_eviction_matrix" in config_params, "config_params is expected to contain 'data_eviction_matrix'"
-        data_eviction_matrix = config_params["data_eviction_matrix"]
+            # Input parameters validation
+            assert isinstance(tiers, list) and all(list(map(lambda a: isinstance(a, str), tiers))), \
+                "tiers must be a list of strings. Got: {}".format(tiers)
 
-        # Input parameters validation
-        assert isinstance(tiers, list) and all(list(map(lambda a: isinstance(a, str), tiers))), \
-            "tiers must be a list of strings. Got: {}".format(tiers)
+            assert isinstance(data_admission_matrix, list), "data_admission_matrix must be a list. Got: {}".format(data_admission_matrix)
+            assert isinstance(data_eviction_matrix, list), "data_eviction_matrix must be a list. Got: {}".format(data_admission_matrix)
 
-        assert isinstance(data_admission_matrix, list), "data_admission_matrix must be a list. Got: {}".format(data_admission_matrix)
-        assert isinstance(data_eviction_matrix, list), "data_eviction_matrix must be a list. Got: {}".format(data_admission_matrix)
+            assert len(tiers) == len(data_admission_matrix), \
+                "data admission matrix must have one sublist per tiers entry. Got: {}".format(data_admission_matrix)
+            assert len(tiers) == len(data_eviction_matrix), \
+                "data eviction matrix must have one sublist per tiers entry. Got: {}".format(data_eviction_matrix)
 
-        assert len(tiers) == len(data_admission_matrix), \
-            "data admission matrix must have one sublist per tiers entry. Got: {}".format(data_admission_matrix)
-        assert len(tiers) == len(data_eviction_matrix), \
-            "data eviction matrix must have one sublist per tiers entry. Got: {}".format(data_eviction_matrix)
+            assert all(list(map(lambda a: isinstance(a, list) and len(a) == len(tiers) and \
+                all(list(map(lambda b: isinstance(b, float), a))) and \
+                sum(a) - 1.0 < 10 ** -6, data_admission_matrix))), \
+                "All data_admission_matrix values must be lists with one float value per tier adding up to a 1.0 Got: {}".format(data_admission_matrix)
 
-        assert all(list(map(lambda a: isinstance(a, list) and len(a) == len(tiers) and \
-            all(list(map(lambda b: isinstance(b, float), a))) and \
-            sum(a) - 1.0 < 10 ** -6, data_admission_matrix))), \
-            "All data_admission_matrix values must be lists with one float value per tier adding up to a 1.0 Got: {}".format(data_admission_matrix)
+            assert all(list(map(lambda a: isinstance(a, list) and len(a) == len(tiers) and \
+                all(list(map(lambda b: isinstance(b, float), a))) and \
+                sum(a) - 1.0 < 10 ** -6, data_eviction_matrix))), \
+                "All data_eviction_matrix values must be lists with one float value per tier adding up to a 1.0 Got: {}".format(data_eviction_matrix)
 
-        assert all(list(map(lambda a: isinstance(a, list) and len(a) == len(tiers) and \
-            all(list(map(lambda b: isinstance(b, float), a))) and \
-            sum(a) - 1.0 < 10 ** -6, data_eviction_matrix))), \
-            "All data_eviction_matrix values must be lists with one float value per tier adding up to a 1.0 Got: {}".format(data_eviction_matrix)
+            # Populate the member variables
+            self.tiers = tiers
+            self.tier_index = {}
+            for i in range(len(tiers)):
+                self.tier_index[tiers[i]] = i
 
-        # Populate the member variables
-        self.tiers = tiers
-        self.tier_index = {}
-        for i in range(len(tiers)):
-            self.tier_index[tiers[i]] = i
-
-        self.data_admission_matrix = data_admission_matrix
-        self.data_eviction_matrix = data_eviction_matrix
+            self.data_admission_matrix = data_admission_matrix
+            self.data_eviction_matrix = data_eviction_matrix
 
     def __str__(self):
         return "ProbabilityBasedDataMigrationPolicy(tiers={}, data_admission_matrix={}, data_eviction_matrix={})".format(
@@ -114,7 +120,16 @@ class ProbabilityBasedDataMigrationPolicy(AbstractDataMigrationPolicy):
         return self.tiers[-1]
 
     def persist_to_file(self, file_name):
-        raise NotImplementedError
+        """We save the contents of the tiers, tier_index, data_admission_matrix and data_eviction_matrix to the file."""
+
+        import json
+        with open(file_name, 'w') as f:
+            json.dump({
+                'tiers': self.tiers,
+                'tier_index': self.tier_index,
+                'data_admission_matrix': self.data_admission_matrix,
+                'data_eviction_matrix' : self.data_eviction_matrix
+            }, f)
 
 if __name__ == "__main__":
 
@@ -125,6 +140,17 @@ if __name__ == "__main__":
 
     DEFAULT_ADMISSION_EVALUATIONS = ["SSD", 1000, "NVM", 1000, "RAM", 1000]
     DEFAULT_EVICTION_EVALUATIONS = ["SSD", 1000, "NVM", 1000, "RAM", 1000]
+
+    # Simple testing space
+    bps = ProbabilityBasedDataMigrationPolicy({
+        "tiers": DEFAULT_TIERS,
+        "data_admission_matrix" : [[0.0, 0.5, 0.5], [0.0, 0.1, 0.9], [0.0, 0.0, 1.0]],
+        "data_eviction_matrix" : [[1.0, 0.0, 0.0], [0.9, 0.1, 0.0], [0.5, 0.5, 0.0]]})
+    print(bps)
+    bps.persist_to_file('test.json')
+    bps = ProbabilityBasedDataMigrationPolicy(init_from_file='test.json')
+    print(bps)
+    """
 
     # Parse given arguments
     import argparse
@@ -196,3 +222,4 @@ if __name__ == "__main__":
     for i in range(0, len(args.eviction_evaluations), 2):
         for j in range(args.eviction_evaluations[i + 1]):
             print("evict,{},{}".format(args.eviction_evaluations[i], bps.destination_on_eviction_from(args.eviction_evaluations[i])))
+    """
