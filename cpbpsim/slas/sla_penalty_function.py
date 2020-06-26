a7e606b09ce71c37dc7e304ff77d90fc45a5791c
Taras Basiuk <BasiukTV@gmail.com>
2020-06-26 17:20:31 -0500

    Implementing the to-file persistance of the piece-wise linear SLA.

--- cpbpsim/slas/sla_penalty_function.py
+++ cpbpsim/slas/sla_penalty_function.py
@@ -7,8 +7,10 @@ class AbstractSLAPenaltyFunction(ABC):
     """Abstract class for SLA violation penalty functions."""
 
     @abstractmethod
-    def __init__(self, slo_type, eval_period, mapping_func, logger):
-        """Initialize the function with the given SLO type, evaluation period and mapping function."""
+    def __init__(self, logger, slo_type, eval_period, mapping_func, init_from_file=None):
+        """
+            Initialize the function with the given SLO type, evaluation period and mapping function.
+        """
         pass
 
     @abstractmethod
@@ -21,19 +23,54 @@ class AbstractSLAPenaltyFunction(ABC):
         """Evaluate the SLA violation penalty corresponding to recorded SLO values starting form given time."""
         pass
 
+    @abstractmethod
+    def persist_to_file(self, file_name, include_recordings=False):
+        """
+            Persists the state of the agreement to a file, from which it can be later recovered.
+            Optionally, include the historical recordings of the SLO as well.
+        """
+        pass
+
 class AveragingPiecewiseLinearSLAPenaltyFunction(AbstractSLAPenaltyFunction):
     """Implementation of the AbstractSLAPenaltyFunction based on the piecewise linear mapping function."""
 
-    def __init__(self, slo_type, eval_period, mapping_func, logger):
+    def __init__(self, logger, slo_type=None, eval_period=None, mapping_func=None, init_from_file=None):
         """
+            Option 1:
             Constructor whose mapping function has baseline_slope and baseline_intercept float values
             which are used for the range from minus infinity to the first (smallest) given interval starting point.
 
             Also mapping function has a dictionary of intervals, where keys are float interval starting points,
             and values are pairs of linear slopes and intercepts to be used for the corresponding interval.
 
-            Finally, SLO type string and evaluation period integer are expected.
+            Finally, SLO type string, evaluation period integer, and a logger are expected.
+
+            Option 2:
+            Constructor which takes a logger and a init_from_file JSON file name from which
+            all of the parameters for Option 1 are taken.
         """
+        assert (slo_type != None and eval_period != None and mapping_func != None) != (init_from_file != None), \
+            "Either slo_type and eval_period and mapping_func must be given or a JSON init_from_file path."
+
+        slo_record_timestamps = []
+        slo_recordings = {}
+
+        # If the init_from_file is given, initialize slo_type, eval_period, mapping_func, slo_record_timestamps, slo_recordings from it.
+        if init_from_file:
+            import json
+            with open(init_from_file) as f:
+                persisted_state = json.load(f)
+                slo_type = persisted_state['slo_type']
+                eval_period = persisted_state['eval_period']
+                slo_record_timestamps = persisted_state['slo_record_timestamps']
+                slo_recordings = dict(map(lambda kv: (int(kv[0]), kv[1]), persisted_state['slo_recordings'].items()))
+
+                mapping_func = {}
+                mapping_func['baseline_slope'] = persisted_state['baseline_slope']
+                mapping_func['baseline_intercept'] = persisted_state['baseline_intercept']
+                mapping_func['intervals'] = {}
+                for k, v in persisted_state['intervals'].items():
+                    mapping_func['intervals'][float(k)] = (v[0], v[1])
 
         # Validates the constructor arguments
         assert isinstance(slo_type, str), "slo_type must be a string. Got: {}".format(slo_type)
@@ -67,8 +104,8 @@ class AveragingPiecewiseLinearSLAPenaltyFunction(AbstractSLAPenaltyFunction):
         self.logger = logger
 
         # Prepare a list of SLO recording timestamps and a dictionary of corresponding SLO values
-        self.slo_record_timestamps = []
-        self.slo_recordings = {}
+        self.slo_record_timestamps = slo_record_timestamps
+        self.slo_recordings = slo_recordings
 
     def __str__(self):
         return "AdditivePiecewiseLinearSLAPenaltyFunction(slo_type={}, eval_period={}, base_slope={}, base_intercept={}, intervals={}, slo_recordings={})".format(
@@ -82,7 +119,7 @@ class AveragingPiecewiseLinearSLAPenaltyFunction(AbstractSLAPenaltyFunction):
         self.slo_record_timestamps.append(timestamp)
         self.slo_recordings[timestamp] = slo_val
 
-    def eval_penalty(self, from_time):
+    def eval_penalty(self, from_time=0):
         """
             Evaluates the penalty corresponding to the recorded SLO values from a given timestamp.
 
@@ -137,6 +174,27 @@ class AveragingPiecewiseLinearSLAPenaltyFunction(AbstractSLAPenaltyFunction):
         # Return the total SLA penalties accumulated since the from_time
         return sum(sla_penalties)
 
+    def persist_to_file(self, file_name, include_recordings=False):
+        """
+            We save the contents of the slo_type, eval_period, intervals, starting_points, baseline_slope
+            and baseline_intercept to the file.
+
+            Optionally, we also save the slo_record_timestamps and slo_recordings to the file.
+        """
+
+        import json
+        with open(file_name, 'w') as f:
+            json.dump({
+                'slo_type': self.slo_type,
+                'eval_period': self.eval_period,
+                'intervals': self.intervals,
+                'starting_points' : self.starting_points,
+                'baseline_slope': self.baseline_slope,
+                'baseline_intercept' : self.baseline_intercept,
+                'slo_record_timestamps' : self.slo_record_timestamps if include_recordings else [],
+                'slo_recordings' : self.slo_recordings if include_recordings else {}
+            }, f)
+
 if __name__ == "__main__":
     # If executed, the file will evaluate the given penalty function on a given range of SLO parameter values
 
@@ -146,8 +204,29 @@ if __name__ == "__main__":
     DEFAULT_BASELINE_SLOPE = 0.0
     DEFAULT_BASELINE_INTERCEPT = 0.0
 
+    # Testing space
+    """
+    import logging
+    sla = AveragingPiecewiseLinearSLAPenaltyFunction(
+        logging.getLogger(__name__),
+        'latency',
+        100,
+        {"intervals" : {0.9 : (0.0, 0.8), 0.99 : (0.0, 1.0)}, "baseline_slope" : 0.0, "baseline_intercept" : 0.0})
+    sla.record_SLO( 50, 0.95)
+    sla.record_SLO(150, 1.0)
+    print(sla)
+    print(sla.eval_penalty())
+    sla.persist_to_file("test.json", include_recordings=True)
+
+    sla = AveragingPiecewiseLinearSLAPenaltyFunction(
+        logging.getLogger(__name__),
+        init_from_file = "test.json")
+    print(sla)
+    print(sla.eval_penalty())
+    """
+
     # Parse given arguments
-    import argparse
+    import argparse, logging
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=
         '''This tool outputs a list of evaluations in given range for a given piecewise linear SLA violation penalty function.''')
 
@@ -165,7 +244,7 @@ if __name__ == "__main__":
 
     # Validate the given parameters
     assert len(args.intervals) % 3 == 0, "Number of arguments to define intervals should be divisible by 3. Got: {}".format(len(args.intervals))
-    assert len(args.intervals) % 2 == 0, "Number of arguments to define SLO value records should be divisible by 2. Got: {}".format(len(args.intervals))
+    assert len(args.slo_val_records) % 2 == 0, "Number of arguments to define SLO value records should be divisible by 2. Got: {}".format(len(args.slo_val_records))
 
     # Prepare the intervals dictionary
     intervals = {}
@@ -173,12 +252,15 @@ if __name__ == "__main__":
         intervals[args.intervals[i]] = (args.intervals[i + 1], args.intervals[i + 2])
 
     # Instantiate the function
-    sla = AveragingPiecewiseLinearSLAPenaltyFunction(args.slo_type, args.eval_period,
-        {"intervals" : intervals, "baseline_slope" : args.baseline[0], "baseline_intercept" : args.baseline[1]})
+    sla = AveragingPiecewiseLinearSLAPenaltyFunction(
+        logging.getLogger(__name__),
+        slo_type = args.slo_type,
+        eval_period = args.eval_period,
+        mapping_func = {"intervals" : intervals, "baseline_slope" : args.baseline[0], "baseline_intercept" : args.baseline[1]})
 
     # Evaluate the SLA on the sequence of SLO value records
     for i in range(0, len(args.slo_val_records), 2):
         sla.record_SLO(args.slo_val_records[i], args.slo_val_records[i + 1])
 
     # Return the result
-    print(sla.eval_penalty(0))
+    print(sla.eval_penalty())
